d1b8a7fde93a37392c1426df37cccfa608acf30e
Choadis <dahlgrensm@gmail.com>
2018-04-24 00:17:35 -0400

    Began work on auth plug, everything seems to be working to this point

--- pokedex/web/controllers/user_controller.ex
+++ pokedex/web/controllers/user_controller.ex
@@ -2,6 +2,8 @@ defmodule Pokedex.UserController do
   use Pokedex.Web, :controller
   alias Pokedex.User
 
+  plug :authenticate when action in [:index, :show]
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
@@ -18,15 +20,24 @@ defmodule Pokedex.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "#{user.name} created!")
-        |> redirect(to: user_path(conn, :index))
-        {:error, changeset} ->
-          render(conn, "new.html", changeset: changeset)
-        end
-      end
-
+      {:ok, user} -> conn
+      |> put_flash(:info, "#{user.name} created!")
+      |> redirect(to: user_path(conn, :index))
+      {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+end
